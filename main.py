074c5f3ff45757b0d1a08a7835564fe7708adfdb
LomasovEgor <egorka.lomasov@gmail.com>
2022-07-28 21:41:01 +0300

    hotfix

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ if __name__ == '__main__':
     logger.info("start getting teachers")
     teachers, failed_ids = Parser.get_teachers(ids)
     logger.info("finish getting teachers")
+    logger.warning(f'{failed_ids=}')
 
     with open('teachers.json', 'w', encoding='utf8') as file:
-        json.dump(ids, file, indent=2, ensure_ascii=False)
+        json.dump(teachers, file, indent=2, ensure_ascii=False)
